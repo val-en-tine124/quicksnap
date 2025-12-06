b4a050a2790ea8228a7ab97194f748a09b7c8944
val-en-tine124 <valentinechibueze400@gmail.com>
2025-12-06 22:54:49 +0100

    chore: Create main,providers module

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
