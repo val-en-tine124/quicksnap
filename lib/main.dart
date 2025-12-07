94bc60d104e1ee3477697e457ca189820b735945
val-en-tine124 <valentinechibueze400@gmail.com>
2025-12-07 11:03:25 +0100

    chore: Add a Material app widget  and providers to this module

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quicksnap/providers.dart';
 
 void main() {
-  
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context,WidgetRef ref) {
+    return MaterialApp(
+      home: HomePage(),
+      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Valentine's QuickSnap")),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(countProvider.notifier).state++;
+        },
+        tooltip: "Increment counter",
+        child: const Icon(Icons.add),
+      ),
+      body: const HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends ConsumerWidget {
+  const HomePageBody({super.key});
+  
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return Center(child:Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("The Current Counter Value is ${ref.watch(countProvider)} "),
+      ],
+    ));
   }
 }
