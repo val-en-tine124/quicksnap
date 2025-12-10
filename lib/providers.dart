ec8224a20a030d3563bce05049199f5a4da024bf
val-en-tine124 <valentinechibueze400@gmail.com>
2025-12-10 21:24:07 +0100

    chore: Create provider for app

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'db.dart';
+import 'models/entry.dart';
 
-var countProvider=StateProvider((ref)=>0);
+final entriesProvider = StreamProvider<List<Entry>>((ref) {
+  return isar.entrys.where().sortByCreatedAtDesc().watch(fireImmediately: true);
+});
+
+final addEntryProvider = Provider((ref) => _AddEntry(ref));
+
+class _AddEntry {
+  final Ref ref;
+  _AddEntry(this.ref);
+
+  Future<void> call({String? text, String? photoPath}) async {
+    final entry = Entry()
+      ..createdAt = DateTime.now()
+      ..text = text?.trim().isNotEmpty == true ? text : null
+      ..photoPath = photoPath;
+
+    await isar.writeTxn(() => isar.entrys.put(entry));
+  }
+}
