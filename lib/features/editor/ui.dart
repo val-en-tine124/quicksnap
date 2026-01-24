6aa339c9695e2e6118e62620d300c3b875fb0308
val-en-tine124 <valentinechibueze400@gmail.com>
2026-01-24 06:46:32 +0100

    refactor:  Add quill contorller to a provider,refactor EditorScaffold build method, add drawers and ListTiles.

--- lib/features/editor/ui.dart
+++ lib/features/editor/ui.dart
@@ -1,16 +1,52 @@
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-late QuillController _controller;
+import 'package:quicksnap/features/editor/providers.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
-class EditorScalfold extends ConsumerWidget {
-  const EditorScalfold({super.key});
+final quillControllerProvider = Provider.autoDispose<QuillController>((ref) {
+  final _controller = QuillController.basic(); // Basic controller
+  ref.onDispose(() {
+    // Dispose the controller when the provider is disposed
+    _controller.dispose();
+  });
+  return _controller;
+});
+
+class EditorScaffold extends ConsumerWidget {
+  const EditorScaffold({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(title: Text("QuickSnap Editor")),
-      body: _Editor(),
+    final fileData = ref.watch(filePickerProvider);
+
+    return fileData.when(
+      data: (data) {
+        final appBarTitle = data?.name ?? "Untitled Document";
+        return Scaffold(
+          appBar: AppBar(title: RepaintBoundary(child: Text(appBarTitle))),
+          body: _Editor(),
+          drawer: Drawer(child: Center(child: const DrawerFsOps())),
+        );
+      },
+      loading: () => Scaffold(
+        appBar: AppBar(title: const Text('Loading...')),
+        body: const Center(child: CircularProgressIndicator()),
+      ),
+      error: (error, stack) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Error: $error'),
+              duration: const Duration(seconds: 7),
+            ),
+          );
+        });
+        return Scaffold(
+          appBar: AppBar(title: const Text('Error')),
+          body: Center(child: Text('Error: $error')),
+        );
+      },
     );
   }
 }
@@ -25,32 +61,31 @@ class _Editor extends ConsumerStatefulWidget {
 }
 
 class _EditorState extends ConsumerState<_Editor> {
-  
+
   @override
   void initState() {
     super.initState();
-    _controller = QuillController.basic();
   }
 
   @override
   Widget build(BuildContext context) {
+    final controller = ref.watch(quillControllerProvider);
     return Column(
       children: [
         Expanded(
           child: QuillEditor.basic(
-            controller: _controller,
+            controller: controller,
             config: QuillEditorConfig(autoFocus: true),
           ),
         ),
 
-        _EditorToolBar(_controller),
+        _EditorToolBar(controller),
       ],
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 }
@@ -77,3 +112,46 @@ class _EditorToolBar extends StatelessWidget {
   }
 }
 
+class DrawerFsOps extends StatelessWidget {
+  const DrawerFsOps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final fileOpsButtons = <Widget>[
+      ListTile(
+        onTap: () {},
+        leading: const Icon(Icons.add),
+        title: const Text("New File"),
+      ),
+      ListTile(
+        onTap: () {},
+        leading: const Icon(Icons.save),
+        title: const Text("Save File"),
+      ),
+      ListTile(
+        onTap: () {},
+        leading: const Icon(Icons.folder_open),
+        title: const Text("Open File"),
+      ),
+      ListTile(
+        onTap: () {},
+        leading: const Icon(Icons.delete),
+        title: const Text("Delete File"),
+      ),
+    ];
+    return ListView(
+      children: [DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Center(
+                child: Text(
+                  'QuickSnap',
+                  style: TextStyle(
+                    fontSize: 24,)).animate().fadeIn(duration: 4.seconds).then().effect().shake(),
+              )),
+                  ...fileOpsButtons.map((entry) => entry),
+    ],
+    );
+  }
+}
