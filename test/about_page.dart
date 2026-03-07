c27fbc8b1ad8100b0978f32b3e85e0e9ae4165e1
val-en-tine124 <valentinechibueze400@gmail.com>
2026-03-07 05:33:05 +0100

    test: Create file for about section

---
+++ test/about_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quicksnap/features/editor/about.dart';
+
+void main() {
+  testWidgets('Find text on selectable area.', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home:AboutPage()));
+    await tester.pump();
+    expect(find.text("Abba"),findsOneWidget);
+    expect(find.text("compact"),findsOneWidget);
+  }); 
+}
