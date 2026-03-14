27e5e0bc9c108ffdcaa4ac16ebd4f908ff681ced
val-en-tine124 <valentinechibueze400@gmail.com>
2026-03-14 13:14:11 +0100

    chore: create file for centralize theming

---
+++ lib/features/editor/theming.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+
+// class QuickTextTheme  extends ThemeExtension<QuickTextTheme>{
+
+//   @override
+//   ThemeExtension<ThemeExtension<dynamic>> copyWith() {
+//     // TODO: implement copyWith
+//     throw UnimplementedError();
+//   }
+
+//   @override
+//   ThemeExtension<ThemeExtension<dynamic>> lerp(covariant ThemeExtension<ThemeExtension<dynamic>>? other, double t) {
+//     // TODO: implement lerp
+//     throw UnimplementedError();
+//   }
+// }
