4404cbc944ee5eef5f387aeddeca6d643f8a0c9a
Your Name <you@example.com>
2026-06-20 17:42:09 +0100

    chore: fixed gitignore and tracked android directory

---
+++ android/app/src/main/kotlin/com/example/quicksnap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quicksnap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import package_info_plus
+import quill_native_bridge_macos
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
